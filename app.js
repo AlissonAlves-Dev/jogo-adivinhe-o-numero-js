71b945b7aa8927211c861ad13b4072c34b58478a
Alisson Alves <133293672+AlissonAlves-Dev@users.noreply.github.com>
2025-05-20 01:23:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let secretNumber = randomNumber();
+let shots = 1;
+
+function showText (tag, text) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = text;
+}
+
+function showTextOnScreen() {
+    showText(".title", "Joguinho do número secreto.");
+    showText(".texto__paragrafo", "Escolha um número entre 1 e 100.")
+}
+
+showTextOnScreen();
+
+function checkShot() {
+    let shot = document.querySelector(".container__input").value;
+    
+    if (secretNumber == shot) {
+        showText(".title", "Você Acertou!");
+
+        let wordShots = shots > 1 ? "tentativas" : "Tentativa";
+        let mesageShots = `Você descobriu o número secreto com um total de ${shots} ${wordShots}!`
+
+        showText(".texto__paragrafo", mesageShots);
+        document.querySelector("#reiniciar").removeAttribute("disabled");
+    } else if (secretNumber < shot){
+        showText(".title", "Você Errou!!!");
+        showText(".texto__paragrafo", "O número secreto é menor do escolhido");
+        clearInput();
+    } else {
+        showText(".title", "Você Errou!!!");
+        showText(".texto__paragrafo", "O número secreto é maior do escolhido");
+        clearInput();
+    }
+    shots++;
+}
+
+function randomNumber() {
+    return parseInt(Math.random() * 100 + 1);
+}
+
+function clearInput() {
+    shot = document.querySelector(".container__input");
+    shot.value = '';
+}
+
+function restart() {
+    secretNumber = randomNumber();
+    shots = 1;
+    showTextOnScreen();
+    clearInput();
+    document.querySelector("#reiniciar").setAttribute("disabled", true);
+}
